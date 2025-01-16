3d0c7193df9ad0112cc1ac6b341a2c90d3c90d6c
ryan <ryan@rkahn.dev>
2025-01-16 16:52:49 +0000

    Added header and book list

---
+++ packages/components/src/book-list/book-list.fixture.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { BookList } from "./book-list";
+
+export default {
+    'Basic': <BookList books={[
+        'Moby Dick',
+        'Oliver Twist'
+    ]} />
+}
---
+++ packages/components/src/header/header.fixture.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Header } from "./header";
+
+export default {
+    'Basic Header': <Header />
+}
