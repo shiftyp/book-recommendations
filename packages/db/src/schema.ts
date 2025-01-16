14cfabbbfc39ad7b10648476281dbd7683cee019
ryan <ryan@rkahn.dev>
2025-01-16 18:44:39 +0000

    Moved database files from frontend to db package. Updated booklist to take an array of books from the database.

---
+++ packages/db/src/schema.ts
@@ -0,0 +1,12 @@
+import { date, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const books = pgTable("books", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    author: varchar({ length: 255 }).notNull(),
+    genre: varchar({ length: 255 }).notNull(),
+    year: integer().notNull()
+});
+
+export type Book = typeof books.$inferSelect
+export type NewBook = typeof books.$inferInsert
